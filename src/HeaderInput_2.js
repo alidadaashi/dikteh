2c72d03708b404657fd4cf9f1e4645de053223dc
alidadaashi <alidadaashi@gmail.com>
2019-10-26 15:49:49 +0330

    update db

--- src/HeaderInput_2.js
+++ src/HeaderInput_2.js
@@ -1,5 +1,22 @@
 import React, { useState } from 'react';
 const HeaderInput_2 = (label, defaultState, placeholder) => {
+
+
+
+
+    const getData = () => {
+        fetch('https://my-json-server.typicode.com/alidadaashi/dikteh/db')
+            // Call the fetch function passing the url of the API as a parameter
+            .then((resp) => resp.json())
+            .then(function (data) {
+                console.log(data)
+                // Your code for handling the data you get from the API
+            })
+            .catch(function () {
+                // This is where you run code if the server returns any errors
+            });
+    }
+
     const [state, updateState] = useState(defaultState);
     const Input = () => (
         <div className="form-group row">
@@ -7,7 +24,11 @@ const HeaderInput_2 = (label, defaultState, placeholder) => {
             <div className="col-sm-10 mr-auto">
                 <input id={label} type="text" className="form-control" placeholder={placeholder}
                     value={state}
-                    onChange={e => updateState(e.target.value)}
+                    onChange={e => {
+                        updateState(e.target.value);
+                        getData();
+                    }
+                    }
                 />
                 <p>{state}</p>
             </div>
--- src/Pet.js
+++ src/Pet.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function Pet(props) {
+
   return (
     <div>
       <h2>
