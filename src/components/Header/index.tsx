9160226d6de73cd2fcb09d0ad801a2d85f300b70
Ali Dadashi <alidadaashi@gmail.com>
2023-07-27 11:20:10 +0300

    add sass and svgs

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,17 @@
 import './header.scss';
+import Image from 'next/image'
+
 export const Header = () => {
     return(
-        <header className="max-w-6xl mx-auto header w-full mb-4">
-            ad
+        <header className="max-w-6xl mx-auto header w-full mb-4 justify-center">
+            <Image
+                className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+                src="/images/barg-emtehani.svg"
+                alt="Next.js Logo"
+                width={180}
+                height={37}
+                priority
+                />
         </header>
     );
 }
