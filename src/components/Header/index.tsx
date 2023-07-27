790d950603aa849e16788c6582a6cb8f2af4d983
Ali <alidadaashi@gmail.com>
2023-07-27 18:08:35 +0300

    Add main section and fonts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,24 @@
 import './header.scss';
-import Image from 'next/image'
 
 export const Header = () => {
     return(
-        <header className="max-w-6xl mx-auto header w-full mb-4 justify-center">
-            <Image
-                className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-                src="/images/barg-emtehani.svg"
-                alt="Next.js Logo"
-                width={180}
-                height={37}
-                priority
-                />
+        <header className="max-w-6xl mx-auto header w-full mb-4 justify-center p-4 pt-10">
+            <div className='nastaliq text-5xl text-center mb-6'>برگ امتحانی</div>
+            <div className="grid grid-cols-3 gap-24">
+
+            <div className="flex flex-col header-label-box  text-right">
+                <div className='nastaliq text-3xl mb-3'> نام</div>
+                <div className='nastaliq text-3xl'>  نام خانوادگی</div>
+            </div>
+            <div className="flex flex-col header-label-box text-right">
+                <div className='nastaliq text-3xl mb-3'> تحصیلات</div>
+                <div className='nastaliq text-3xl'> رشته</div>
+            </div>
+            <div className="flex flex-col header-label-box text-right">
+                <div className='nastaliq text-3xl mb-3' > تاریخ</div>
+                <div className='nastaliq text-3xl'> موضوع</div>
+            </div>
+            </div>
         </header>
     );
 }
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,12 @@
 
+import Quiz from '../Quiz';
+import Timer from '../Timer';
 import './main.scss' 
 export default function Main() {
     return (
-        <div className="max-w-6xl mx-auto w-full main">body  </div>
+        <div className="max-w-6xl mx-auto w-full main  p-7 relative"> 
+            <Quiz />
+            <Timer time='15' />
+        </div>
     );
 }
