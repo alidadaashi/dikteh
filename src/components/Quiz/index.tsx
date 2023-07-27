4ee25e88498469e15508dd140600c7b0681610d3
Ali <alidadaashi@gmail.com>
2023-07-27 22:07:19 +0300

    fix quiz margin

--- src/components/Quiz/index.tsx
+++ src/components/Quiz/index.tsx
@@ -2,7 +2,7 @@ const Quiz = () => {
     return (
         <div className="quiz mt-2">
             <h4 className="text-3xl">گزینه صحیح را انتخاب کنید.</h4>
-            <div className="flex mt-3 justify-center">
+            <div className="flex mt-20 justify-center">
             <a
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
