b332c6dd77504d5f96eae6f2e25dc15eac63c4b4
Ali <alidadaashi@gmail.com>
2023-07-27 11:43:18 +0300

    Add main structure

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
+import './header.scss';
 export const Header = () => {
     return(
-        <header className="max-w-6xl mx-auto header">
+        <header className="max-w-6xl mx-auto header w-full mb-4">
             ad
         </header>
     );
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,7 @@
+
+import './main.scss' 
+export default function Main() {
+    return (
+        <div className="max-w-6xl mx-auto w-full main">body  </div>
+    );
+}
