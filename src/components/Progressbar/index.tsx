e63b1ec937da6887636152c5c2c0f8309528716a
alidadaashi <alidadaashi@gmail.com>
2023-08-01 16:06:44 +0300

    change progressbar colors

--- src/components/Progressbar/index.tsx
+++ src/components/Progressbar/index.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ProgressBar = ({ value, maxValue, level }) => {
   // Calculate the percentage of progress
   const percentage = (value / maxValue) * 100 + 1;
+  const [bgColor, setBgColor] = useState('bg-blue-500');
+  useEffect(() => {
+    switch (level) {
+      case 'ابتدایی':
+        setBgColor('bg-yellow-500');
+        break;
+      case 'راهنمایی':
+        setBgColor('bg-green-500');
+        break;
+      case 'دبیرستان':
+        setBgColor('bg-red-500');
+        break;
+      case 'کنکور':
+        setBgColor('bg-red-500');
+        break;
+      default:
+        setBgColor('bg-blue-500');
+    }
+  }, [level]);
 
   return (
     <div className='relative mx-auto mt-8 h-8 w-2/4 overflow-hidden rounded-lg bg-gray-400'>
       {/* Progress bar */}
       <div
-        className='absolute left-0 top-0 h-full w-0.5 rounded-lg bg-blue-500 transition-all duration-200'
+        className={`absolute left-0 top-0 h-full w-0.5 rounded-lg  transition-all duration-200 ${bgColor}`}
         style={{ width: `${percentage}%` }}
       />
 
